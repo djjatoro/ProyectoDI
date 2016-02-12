d018ecf5f804c7ec37e64d320024d7eeb438f533
Alumno <Alumno@ti2-19>
2016-02-12 20:57:17 +0100

    Version 12-02-16

--- src/modelo/escenario/Celda.java
+++ src/modelo/escenario/Celda.java
@@ -6,18 +6,23 @@ import java.util.LinkedList;
 public class Celda {
     private Posicion posicion;
     private int capacidad;
+    private final int CAPACMAX=5;
+    private boolean ardiendo=false;
     private LinkedList<Celda> celdas;
     private LinkedList<Entidad> entidades;
 
-    //CONSTRUCTOR ja
+    //CONSTRUCTOR
     public Celda(Posicion posicion, int capacidad) {
         this.posicion = posicion;
         this.capacidad = capacidad;
+        celdas = new LinkedList<>();
+        entidades = new LinkedList<>();
     }
+    
     //CONSTRUCTOR, CAPACIDAD CONSTANTE
     public Celda(Posicion posicion) {
         this.posicion = posicion;
-        capacidad=5; 
+        this.capacidad=CAPACMAX;
     }
     
     //GETTER posición
@@ -27,11 +32,68 @@ public class Celda {
     
     //GETER celda vecina
     public Celda getCeldaVecina(Orientacion o){
+        for (Celda c : celdas)
+            if (posicion.adyacente(c.getPosicion().getCoordX(), c.getPosicion().getCoordY(), o))
+                return c;
+        return null;        
+    }
+    
+    public LinkedList<Celda> getCeldasVecinas(){
+        return celdas;
+    }
+    
+    public int getCapacidadLibre(){
+        return capacidad-entidades.size();
+    }
+    
+    public boolean isLlena(){
+        if (capacidad==entidades.size())
+            return false;
+        return true;
+    }
+    
+    public boolean addEntidad(Entidad e){
+        if (capacidad-entidades.size()>0){
+            entidades.add(e);
+            return true;
+        }else
+            return false;
+    }
+    
+    public boolean removeEntidad (Entidad e){
+        return entidades.remove(e);
+    }
+    
+    public void arder(){
+        ardiendo=true;
+    }
+    
+    public void apagar(){
+        ardiendo=false;
+    }
+    
+    public boolean isArdiendo(){
+        return ardiendo;
+    }
+    
+    public boolean conecta(Orientacion o, Celda c){
+        if (posicion.adyacente(c.getPosicion().getCoordX(), c.getPosicion().getCoordY(), o)){
+            celdas.add(c);
+            c.getCeldasVecinas().add(this);
+            return true;
+        } else
+            return false;
+    }
+    
+    public void arrastrar(Orientacion o, int desplazamiento){
         
     }
     
-    public LinkeList<Celda> getCeldasVecinas(){
+    public void empujar(Celda c, Orientacion o, int desplazamiento){
         
     }
     
+    public void colocar (){
+        
+    }
 }
